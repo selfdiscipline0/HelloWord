b4829a19c58a5e3d32accc2219be6f11eb837f34
wei <2417379083@qq.com>
2021-12-02 10:28:50 +0800

    hot-fix提交2

--- git_test/src/main/java/com/wwk/git/GitTest.java
+++ git_test/src/main/java/com/wwk/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,git!");
         System.out.println("hot-fix,git!");
+        System.out.println("hot-fix,test2!");
 
 
     }
