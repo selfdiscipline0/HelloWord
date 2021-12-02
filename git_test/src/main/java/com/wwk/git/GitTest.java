79710df9e7721d93aee21b650b58e0d137d3ee9b
凯小白 <2417379083@qq.com>
2021-12-02 15:42:08 +0800

    ssh登入测试提交

--- git_test/src/main/java/com/wwk/git/GitTest.java
+++ git_test/src/main/java/com/wwk/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hot-fix,git!");
         System.out.println("master,test1!");
         System.out.println("hot-fix,test2!");
+        System.out.println("ssh登入测试,test2!");
 
 
     }
