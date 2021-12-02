796bc77d26c3562aa79b6fd62c1b644ce18d47c7
wei <2417379083@qq.com>
2021-12-02 10:09:20 +0800

    第一次提交

---
+++ git_test/src/main/java/com/wwk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wwk.git;
+
+/**
+ * @author
+ * @date 2021/12/2-10:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
--- src/experiment/Hello.java
+++ src/experiment/Hello.java
@@ -1,5 +1,7 @@
 package experiment;
 
 public class Hello {
-
+    public static void main(String[] args) {
+        System.out.println("git测试！");
+    }
 }
